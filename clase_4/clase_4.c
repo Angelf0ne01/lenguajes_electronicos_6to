3d6941fc35c2a5c06594dece825fb8213131904a
Angel Vazquez <angel.vazquez@extendeal.com>
2021-08-17 19:11:58 -0300

    clase 4

---
+++ clase_4/clase_4.c
@@ -0,0 +1,35 @@
+//clase 4.
+
+//<tipo de retornar> nombre_funcion( los parametros);
+//funcion que retornar un numero entero.
+
+#define DEBUG 1
+int suma(int trigger, int echo){
+    return valor_1+valor_2;
+}
+
+float division(int val1,int valo2){
+    return val1/val2;
+}
+
+void mostrar_un_mensaje(bool debug){
+    int sum= suma(1,2)
+    float div = division(4,4)
+    if(debug){
+        print("el resultado es %d,%f ",sum,div);
+    }
+    
+}
+
+
+void msg(String msg){
+    if(debug){//booleana
+        Serial.println(msg)
+    }
+}
+
+void main(){
+    dato = scanef()
+    lectura_de_sensores(1)
+    mostrar_mensajes();
+}
---
+++ clase_4/ejercicio_5.c
@@ -0,0 +1,47 @@
+//ejercicio 1
+//crear una funcion que recibe por parametro el dia de la seman
+//de forma numerica de 1 a 7
+//y debe retornar el dia de la semana, utilizando arreglos.
+
+
+//output
+//input 1, output lunes
+//input 2, output martes
+//input 3, output miercoles
+
+
+
+
+
+
+
+
+
+
+
+//Ejercicio 2.
+//dato el siguiente conjunto de datos
+//realizar una funcion que me permita escalar x2 el dato recibido
+//por parametros.
+//ejemplo:
+//input 2, output 4
+//input 10, output 20
+//SET DE DATOS:
+//1,5,6,7,2,10
+//output esperado:
+//2,10,12,14,4,20
+int set_de_datos[6]={1,5,6,7,2,10};
+
+int escalar(int dato,int n, char c){
+    return dato*n;
+}
+
+for(int idx=0;idx<6;idx++){
+    escala=2;
+    escalar(set_de_datos[idx],escala)
+}
+
+//Ejercicio 3
+//Realizar una funcion que me permita escalar de forma dinamica.
+
+
